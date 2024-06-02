f7a11762278fa9a13335a258f4a64efe2eb8a73b
mr-mobasheri <mrm26883@gmail.com>
2024-06-02 22:10:59 +0330

    feat: handle POST and DELETE requests

--- src/main/java/HttpHandler/UserHandler.java
+++ src/main/java/HttpHandler/UserHandler.java
@@ -16,64 +16,44 @@ public class UserHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         UserController userController = new UserController();
         String method = exchange.getRequestMethod();
-        String path = exchange.getRequestURI().getPath();
+        String[] pathDetails = exchange.getRequestURI().getPath().split("/");
         String response = "";
-        String[] splittedPath = path.split("/");
         switch (method) {
             case "GET":
-                if (splittedPath.length == 2) {
+                if (pathDetails.length == 2) {
                     response = userController.getUsers();
                 } else {
-                    String userId = splittedPath[splittedPath.length - 1];
+                    String userId = pathDetails[pathDetails.length - 1];
                     response = userController.getUser(userId);
                 }
                 break;
-//            case "POST":
-//                // Read the request body
-//                InputStream requestBody = exchange.getRequestBody();
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(requestBody));
-//                StringBuilder body = new StringBuilder();
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    body.append(line);
-//                }
-//                requestBody.close();
-//
-//                // Process the user creation based on the request body
-//                String newUser = body.toString();
-//                JSONObject jsonObject = new JSONObject(newUser);
-//                try {
-//                    userController.createUser(jsonObject.getString("id"), jsonObject.getString("firstName"), jsonObject.getString("lastName"), jsonObject.getString("email"), jsonObject.getString("phoneNumber"), jsonObject.getString("password"), jsonObject.getString("country"), new Date(jsonObject.getLong("birthday")));
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                Files.createDirectories(Paths.get("src/main/java/com/sinarmin/server/assets/" + jsonObject.getString("id")));
-//                response = "this is done!";
-//                break;
-//            case "PUT":
-//                response = "This is the response users Put";
-//                break;
-//            case "DELETE":
-//                if (splittedPath.length == 2) {
-//                    try {
-//                        userController.deleteUsers();
-//                        response = "All users deleted";
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                } else {
-//                    // Extract the user ID from the path
-//                    String userId = splittedPath[splittedPath.length - 1];
-//                    try {
-//                        userController.deleteUser(userId);
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                response = "This is the response users Delete\nDone!";
-//                break;
-//            default:
-//                break;
+            case "POST":
+                // Read the request body as String.
+                InputStream requestBodyStream = exchange.getRequestBody();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(requestBodyStream));
+                StringBuilder requestBody = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    requestBody.append(line);
+                }
+                requestBodyStream.close();
+                // Process the user creation based on the request body.
+                JSONObject jsonObject = new JSONObject(requestBody);
+                userController.createUser(jsonObject.getString("username"), jsonObject.getString("password"),
+                        jsonObject.getString("firstname"), jsonObject.getString("lastname"),
+                        jsonObject.getString("email"), jsonObject.getString("phoneNumber"),
+                        jsonObject.getString("country"), Integer.parseInt(jsonObject.getString("birthday")));
+                response = "New user saved!";
+                break;
+            case "DELETE":
+                if (pathDetails.length == 2) {
+                    userController.deleteAllUsers();
+                } else {
+                    // get the username from the path and delete it.
+                    userController.deleteUser(pathDetails[pathDetails.length - 1]);
+                }
+                response = "User deleted successfully";
+                break;
         }
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
